cdfffe12935671d6bf63c9ca3ffc43cf8d7796e6
elichad <eli.chadwick.256@gmail.com>
2022-03-24 12:46:32 +0000

    update in line with other viz

--- config/plugins/visualizations/crystvis/src/crystvis.js
+++ config/plugins/visualizations/crystvis/src/crystvis.js
@@ -3,36 +3,35 @@ import * as _ from "underscore";
 import * as crystvis from "crystvis-js";
 import { request as requestDatasets } from "@galaxyproject/charts/lib/utilities/datasets";
 
-_.extend(window.bundleEntries || {}, {
-    load: function (options) {
-        console.debug("targets are:");
-        console.debug(options.target);
-        var visualiser = new crystvis.CrystVis('#' + options.target, 640, 480);
-        $.ajax({
-            url: options.dataset.download_url,
-            success: function (response) {
-                var name = options.dataset.name;
-                var ext = options.dataset.file_ext;
-                var settings = options.chart.settings;
-                var representation_parameters = {
-                    atom_labels: settings.get("atom_labels"),
-                };
-                var loaded = visualiser.loadModels(response, ext, name);
-                console.debug("loaded model:");
-                console.debug(loaded);
-                visualiser.displayModel(Object.keys(loaded)[0]);
-                console.debug("visualiser:");
-                console.debug(visualiser);
-                visualiser.displayed = visualiser.model.find({
-                    'all': []
-                });
-                setParameters(visualiser, representation_parameters);
-                options.chart.state('ok', 'testing');
-                options.process.resolve();
-            }
-        });
-    }
-});
+window.bundleEntries = window.bundleEntries || {};
+window.bundleEntries.load = function (options) {
+    console.debug("targets are:");
+    console.debug(options.target);
+    var visualiser = new crystvis.CrystVis('#' + options.target, 640, 480);
+    $.ajax({
+        url: options.dataset.download_url,
+        success: function (response) {
+            var name = options.dataset.name;
+            var ext = options.dataset.file_ext;
+            var settings = options.chart.settings;
+            var representation_parameters = {
+                atom_labels: settings.get("atom_labels"),
+            };
+            var loaded = visualiser.loadModels(response, ext, name);
+            console.debug("loaded model:");
+            console.debug(loaded);
+            visualiser.displayModel(Object.keys(loaded)[0]);
+            console.debug("visualiser:");
+            console.debug(visualiser);
+            visualiser.displayed = visualiser.model.find({
+                'all': []
+            });
+            setParameters(visualiser, representation_parameters);
+            options.chart.state('ok', 'testing');
+            options.process.resolve();
+        }
+    });
+};
 
 function setParameters(visualiser, parameters) {
     if (parameters["atom_labels"] == "on") {
