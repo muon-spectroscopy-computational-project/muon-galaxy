714e76cc512fc1f4aba1da7490f8956589a71140
Nalin Gupta <nalin.gupta@stfc.ac.uk>
2024-03-01 11:48:33 +0000

    Added test for issue #33 fix

--- lib/galaxy/datatypes/molecules.py
+++ lib/galaxy/datatypes/molecules.py
@@ -1023,6 +1023,9 @@ class CIF(GenericMolFile):
         >>> fname = get_test_fname('1.star')
         >>> CIF().sniff(fname)
         False
+        >>> fname = get_test_fname('LaMnO3.cif')
+        >>> CIF().sniff(fname)
+        True
         """
 
         # check for optional CIF version marker '#\#CIF_<version>' at start of file
